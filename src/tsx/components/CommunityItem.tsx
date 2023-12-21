155031b26f266504c6b51479d296d01d5c2a6821
Dmytro Kovalenko <76845911+DmytroKalenko@users.noreply.github.com>
2023-12-21 16:42:41 -0700

    feat: Added CommunityItem component

--- src/tsx/components/CommunityItem.tsx
+++ src/tsx/components/CommunityItem.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import {Link} from "react-router-dom";
 type Props = {
-    id: string, name: string, imgUrl: string, group: string,
+    id: string, name: string, imgUrl: string, group: string, homes : []
 }
 export default function CommunityItem(props:Props) {
-    const {id,name,imgUrl,group} = props
+    const {id,name,imgUrl,group,homes} = props
+    const prices = homes.map(({...item} : any )=>{ return item.price })
+    const averagePrice = prices.reduce((a, b) => a + b, 0) / prices.length;
+
+
     return(
         <div className={'communityItem'}>
             <img src={imgUrl} alt={name}/>
-
+            <Link className={'communityItem__title'} to={`/home/${id}`} title={name}>{name}</Link>
+            <p>Belong to {group} community </p>
+            <p>Average price {averagePrice}</p>
         </div>
     )
 }
--- src/tsx/pages/Communities.tsx
+++ src/tsx/pages/Communities.tsx
@@ -1,14 +1,21 @@
-import {ReactElement, ReactNode, useEffect, useMemo, useState} from "react";
+import {useEffect, useState} from "react";
 import CommunityItem from "../components/CommunityItem";
 
 
 interface CommunitiesItem {
-    id: string, name: string, imgUrl: string, group: string
+    id: string,
+    name: string,
+    imgUrl: string,
+    group: string
 }
-interface CommunitiesItems extends Array<CommunitiesItem>{}
+
+interface CommunitiesItems extends Array<CommunitiesItem> {
+}
+
 export default function Communities() {
-    const [data, setData] = useState([] as Array<object>);
-    const toSortCommunities = (communities: CommunitiesItems):object[]=> {
+    const [communities, setCommunities] = useState([] as Array<object>);
+    const [homes, setHomes] = useState([] as Array<object>);
+    const toSortCommunities = (communities: CommunitiesItems): object[] => {
         return communities.sort((a, b) => {
             if (a.group.toUpperCase() < b.group.toUpperCase()) {
                 return -1;
@@ -19,26 +26,51 @@ export default function Communities() {
             return 0;
         })
     }
+    const toGetCommunities = async () => {
+        const response = await fetch('https://storage.googleapis.com/openhouse-ai-fe-coding-test/communities.json');
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Something went wrong');
+    }
+    const toGetHomes = async () => {
+        const response = await fetch('https://storage.googleapis.com/openhouse-ai-fe-coding-test/homes.json');
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Something went wrong');
+    }
+
     useEffect(() => {
-        fetch('https://storage.googleapis.com/openhouse-ai-fe-coding-test/communities.json').then((response) => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error('Something went wrong');
-        })
+        toGetCommunities()
             .then((responseJson) => {
                 const sortedData = toSortCommunities(responseJson)
-                setData(sortedData)
+                setCommunities(sortedData)
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+
+        toGetHomes()
+            .then((responseJson) => {
+                setHomes(responseJson)
             })
             .catch((error) => {
                 console.log(error)
             });
     }, []);
 
+    const toGetCommunityHouses = (communityId: string): object[] => {
+        return homes.filter(({...item}: any) => {
+            return item.communityId.toLowerCase() === communityId.toLowerCase()
+        })
+    }
+
     return (
         <div className={'communitiesPage'}>
-            {data.map(({...item}: any ,index)=>{
-                return <CommunityItem {...item} key={index}/>
+            {communities.map(({...item}: any, index) => {
+                const homes = toGetCommunityHouses(item.id)
+                return <CommunityItem {...item} key={index} homes={homes}/>
             })}
         </div>
     )
